5b3996556353cd6d5ae81444f38f049d6838387d
Aniket Mandal <144683887+AniketMdl@users.noreply.github.com>
2026-06-07 10:38:50 +0530

    new coddmit in server.js

--- server/server.js
+++ server/server.js
@@ -15,3 +15,10 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
+
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => console.log("MongoDB connected"))
+    .catch(err => console.error(err));
